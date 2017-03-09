98e491597b18acfcf555f4565ec366c896ba9a77
josh <josh@joshpmitchell.ca>
2017-03-08 22:46:49 -0500

    Add clarifying comments to ArithmeticExpression.cpp

--- ArithmeticExpression.cpp
+++ ArithmeticExpression.cpp
@@ -103,7 +103,8 @@ string ArithmeticExpression::evaluate(string &INPUT){
 					string newvalue = myMultiplication.evaluate(*left, *right);
 					int Length= temp.size()-length;//create new size
 					int Length1=b-0;//grabbing size
-					temp=temp.substr(0,Length1)+newvalue+temp.substr(length,Length); //create substring
+					//Create substring of remainder of expression
+					temp=temp.substr(0,Length1)+newvalue+temp.substr(length,Length);
 				}
 
 				else if(((temp.substr(a,1)).compare("/")==0)){//else if division operator found
@@ -112,14 +113,15 @@ string ArithmeticExpression::evaluate(string &INPUT){
 					string newvalue = myDivision.evaluate(*left, *right);//grab division
 					int Length= temp.size()-length;//create new size
 					int Length1=b-0; //grabbing size
-					temp=temp.substr(0,Length1)+newvalue+temp.substr(length,Length);//create substring
+					//Create substring of remainder of expression
+					temp=temp.substr(0,Length1)+newvalue+temp.substr(length,Length);
 				} if
 			}
         }
         //ADDITION AND SUBSTRACTION
-        bool found1=false;//second use of found
+        bool found1=false;//create second found flag
 
-        while(!found1){//while add or substract
+        while(!found1){//while adding or substracting
         if (temp.size()>1){
             for(int k=0; k<(temp.size()-1);k++){
                 if(temp.substr(k,2)=="--"){
@@ -130,53 +132,60 @@ string ArithmeticExpression::evaluate(string &INPUT){
         found1=true;
         int b=0;//accumulator
         for(int i=1;i<temp.size();i++){
-            if((((temp.substr(i,1)).compare("+")==0) || ((temp.substr(i,1)).compare("-")==0)) && found1){//if symbol
+			//if the character inspected is an operator (+, -, *, /)
+            if((((temp.substr(i,1)).compare("+")==0) || ((temp.substr(i,1)).compare("-")==0)) && found1){
                 b=i;
                 found1=false; 
             }
         }
-        if(found1==false){ //if false
+        if(found1==false){
             int c=0;
             int newsize=temp.size();//getting size
 
 
             for(int i=1;i<b;i++){
+				//if the character inspected is an operator (+, -, *, /)
                 if(((temp.substr(i,1)).compare("+")==0) || ((temp.substr(i,1)).compare("-")==0) || ((temp.substr(i,1)).compare("*")==0) || ((temp.substr(i,1)).compare("/")==0)){
-                    //if symbol
+
                     c=i+1; //set and add one
                 } 
             }
             int sizenew1=b-c;
-            string A3=temp.substr(c,sizenew1);//creat string
+            string A3=temp.substr(c,sizenew1);//create string
             left=&A3;
             bool boolean=true;
             for(int i=b+1;i<temp.size();i++){
+				//if the character inspected is an operator (+, -, *, /)
                 if((temp.substr(i,1)=="+" || temp.substr(i,1)=="-" || temp.substr(i,1)=="*" || temp.substr(i,1)=="/")&&boolean){
-                        //if symbol
+
                     newsize=i; 
                     boolean=false;
                 }
             }
 
 
-            int newlength=newsize-(b+1);//get size
-            string A4=temp.substr(b+1,newlength);//substring
-            right=&A4;//get right
+            int newlength=newsize-(b+1);//create new size
+            string A4=temp.substr(b+1,newlength);//create substring of newLength
+            right=&A4;//create pointer for right side of new string
 
-            if(temp.substr(b,1)=="+"){ //iff add
-                Addition myAddition; //create object
+            if(temp.substr(b,1)=="+"){ //if addition operator found
+				//Evaluate sum
+                Addition myAddition;
                 string newvalue1 = myAddition.evaluate(*left, *right);// grab at pointers
                 int sizes= c-0;//get size
                 int size1=temp.size()-newsize;//get size
-                temp=temp.substr(0,sizes)+newvalue1+temp.substr(newsize,size1);//create string
+				//Create substring of remainder of expression
+                temp=temp.substr(0,sizes)+newvalue1+temp.substr(newsize,size1);
             }
 
-            else if(temp.substr(b,1)=="-"){ //else if minue
-                Subtraction mySubtraction; //create item of class
-                string newvalue1 = mySubtraction.evaluate(*left, *right);//send withpointers
+            else if(temp.substr(b,1)=="-"){ //else //if subtraction operator found
+				//Evaluate Difference
+                Subtraction mySubtraction;
+                string newvalue1 = mySubtraction.evaluate(*left, *right);
                 int sizes= c-0;//get size
                 int size1=temp.size()-newsize;//get size
-                temp=temp.substr(0,sizes)+newvalue1+temp.substr(newsize,size1);// substring of remainder of expression
+				//Create substring of remainder of expression
+                temp=temp.substr(0,sizes)+newvalue1+temp.substr(newsize,size1);
             } if
 
         }
@@ -184,141 +193,163 @@ string ArithmeticExpression::evaluate(string &INPUT){
         }
         int Lengthleft= leftbracket-0;
         int Lengthright=INPUT.size()-(rightbracket+1);
-        INPUT=INPUT.substr(0,Lengthleft)+temp+INPUT.substr(rightbracket+1,Lengthright);//create new smaller strring
+		//Create substring of remainder of expression
+        INPUT=INPUT.substr(0,Lengthleft)+temp+INPUT.substr(rightbracket+1,Lengthright);
     }
 
 
 
-    else{//if there is no more brackets
-        bool Found=false;
-        while(!Found){//not not true
-        Found=true;
-        int a=0;
-        for(int i=0;i<INPUT.size();i++){
-            if((INPUT.at(i)=='*' || INPUT.at(i)=='/') && Found){//if symbol
-                a=i;
-                Found=false; //set false
-            }
-        }
-        if(Found==false){ //if no mult or div
-            int b=0;
-            int newsize=INPUT.size();
+    else{//if there are no more brackets
+			bool Found=false;
+			while(!Found){
+				Found=true;
+				int a=0;
+				for(int i=0;i<INPUT.size();i++){
+					//if multiplication or division operator found
+					if((INPUT.at(i)=='*' || INPUT.at(i)=='/') && Found){
+						a=i;
+						Found=false;
+					}
+				}
+				if(Found==false){
+					int b=0;
+					int newsize=INPUT.size();
 
-            for(int i=1;i<a;i++){
-                if(INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/'){//if symbol
-                    b=i+1;
-                }
-            }
-            int lens=a-b;//size
+					for(int i=1;i<a;i++){
+						//if the character inspected is an operator (+, -, *, /)
+						if(INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/'){
+							b=i+1;
+						}
+					}
+					int lens=a-b;//size
 
-            string A11=INPUT.substr(b,lens);//string with size
-            left=&A11;//pointer
+					string A11=INPUT.substr(b,lens);//string with size
+					left=&A11;//pointer
 
-            bool fix2=true;//while trye
-            for(int i=a+2;i<INPUT.size();i++){
-                if((INPUT.at(i)=='+'|| INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/')&&fix2){//if symbol
-                    newsize=i;
-                    fix2=false;//set false
-                }
-            }
-            int lenss=newsize-(a+1);//size
+					bool fix2=true;
+					for(int i=a+2;i<INPUT.size();i++){
+						//if the character inspected is an operator (+, -, *, /)
+						if((INPUT.at(i)=='+'|| INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/')&&fix2){
+							newsize=i;
+							fix2=false;
+						}
+					}
+					int lenss=newsize-(a+1);
+					string A22=INPUT.substr(a+1,lenss);
+					right = &A22;
 
-            string A22=INPUT.substr(a+1,lenss);//substring
-            right = &A22;//pointer
-
-            if(INPUT.at(a)=='*'){//if mult
-                Multiplication myMultiplication;//object create
-                string Newvalue = myMultiplication.evaluate(*left, *right);//call
-                int len1=b-0;//get size
-                int len2=INPUT.size()-newsize;//get size
-                INPUT=INPUT.substr(0,len1)+Newvalue+INPUT.substr(newsize,len2);//create string
-            }
-
-            else if(INPUT.at(a)=='/'){//if division
-                Division myDivision;//creat object
-                string Newvalue = myDivision.evaluate(A11, A22);//eval
-                int len1=b-0;//size
-                int len2=INPUT.size()-newsize;//size
-                INPUT=INPUT.substr(0,len1)+Newvalue+INPUT.substr(newsize,len2);//substring
-            } if
-
-
-        }//end
-        }//end
-        //down here dealing with adding/sub
-
-        bool Found1=false;// if false
-        while(!Found1){//while false
-        if (INPUT.size()>1){
-            for(int k=0;k<(INPUT.size()-1);k++){
-                if(INPUT.substr(k,2)=="--"){
-                    INPUT=INPUT.substr(0,k)+"+"+INPUT.substr(k+2);
-                }
-            }
-        }
+					//if the character inspected is a multiplication operator (*)
+					if(INPUT.at(a)=='*'){
+						//Evaluate product
+						Multiplication myMultiplication;
+						string Newvalue = myMultiplication.evaluate(*left, *right);
+						int len1=b-0;//get size
+						int len2=INPUT.size()-newsize;//get size
+						//Create substring of remainder of expression
+						INPUT=INPUT.substr(0,len1)+Newvalue+INPUT.substr(newsize,len2);
+					}
+					
+					//if the character inspected is a division operator (/)
+					else if(INPUT.at(a)=='/'){
+						//Evaluate quotient
+						Division myDivision;
+						string Newvalue = myDivision.evaluate(A11, A22);
+						int len1=b-0;//size
+						int len2=INPUT.size()-newsize;//size
+						//Create substring of remainder of expression
+						INPUT=INPUT.substr(0,len1)+Newvalue+INPUT.substr(newsize,len2);
+					} if
 
 
-        Found1=true;
-        int a=0;
-        for(int i=1;i<INPUT.size();i++){
-            if((INPUT.at(i)=='+' || INPUT.at(i)=='-') && Found1){// if symbol
-                a=i;
-                Found1=false;//set false
-            }
-        }// for
-        if(Found1==false){// if false
-            int b=0;//set zero
-            int c=INPUT.size();// grab size
+				}
+			}
 
-            for(int i=1;i<a;i++){
-                if(INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/'){//if symbol
-                    b=i+1;//add
-                }
-            }
-
-            int lengthh=a-b;//get size
-            string A13=INPUT.substr(b,lengthh);//get string
-            left=&A13;//get pointer
-
-            bool fix3=true;//set true
-            for(int i=a+1;i<INPUT.size();i++){
-                if((INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/')&& fix3){//if symbol
-                    c=i;
-                    fix3=false;//set false
-                }
-            }
-            int llen=c-(a+1);//get size
-            string A14=INPUT.substr(a+1,llen);//get string
-            right =&A14;//pointer
-
-            if(INPUT.at(a)=='+'){//if plus
-                Addition myAddition;//object
-                string value = myAddition.evaluate(A13, A14);//get value
-                int len1=b-0;//get size
-                int len2=INPUT.size()-c; //get size
-                INPUT=INPUT.substr(0,len1)+value+INPUT.substr(c,len2);//get string
-            }
-
-            else if(INPUT.at(a)=='-'){//else if sub
-                Subtraction mySubtraction;//object
-                string value = mySubtraction.evaluate(*left, *right);//evaluate
-                int len1=b-0;//size
-                int len2=INPUT.size()-c;//size
-                INPUT=INPUT.substr(0,len1)+value+INPUT.substr(c,len2);//string
-            }
+			bool Found1=false;
+			while(!Found1){
+			if (INPUT.size()>1){
+				for(int k=0;k<(INPUT.size()-1);k++){
+					if(INPUT.substr(k,2)=="--"){
+						INPUT=INPUT.substr(0,k)+"+"+INPUT.substr(k+2);
+					}
+				}
+			}
 
 
-        }//end
-    }//end
+			Found1=true;
+			int a=0;
+			for(int i=1;i<INPUT.size();i++){
+				//if the character inspected is an addition or subtraction operator (+, -)
+				if((INPUT.at(i)=='+' || INPUT.at(i)=='-') && Found1){
+					a=i;
+					Found1=false;
+				}
+			}
+			if(Found1==false){
+				int b=0;
+				int c=INPUT.size();// grab size
 
-    }//end
+				for(int i=1;i<a;i++){
+					//if the character inspected is an operator (+, -, *, /)
+					if(INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/'){
+						b=i+1;
+					}
+				}
+
+				int lengthh=a-b;//create new size
+				string A13=INPUT.substr(b,lengthh);//create substring of new size
+				left=&A13;//create pointer to left side of new substring
+
+				bool fix3=true;
+				for(int i=a+1;i<INPUT.size();i++){
+					//if the character inspected is an operator (+, -, *, /)
+					if((INPUT.at(i)=='+' || INPUT.at(i)=='-' || INPUT.at(i)=='*' || INPUT.at(i)=='/')&& fix3){
+						c=i;
+						fix3=false;
+					}
+				}
+				int llen=c-(a+1);//create new size
+				string A14=INPUT.substr(a+1,llen);//create substring of new size
+				right =&A14;//create pointer to the right side of new substring
+
+				//if the character inspected is an addition operator (+)
+				if(INPUT.at(a)=='+'){
+					//Evaluate sum
+					Addition myAddition;//object
+					string value = myAddition.evaluate(A13, A14);
+					int len1=b-0;//get size
+					int len2=INPUT.size()-c; //get size
+					//Create substring of remainder of expression
+					INPUT=INPUT.substr(0,len1)+value+INPUT.substr(c,len2);
+				}
+
+				//if the character inspected is a subtraction operator (-)
+				else if(INPUT.at(a)=='-'){
+					//Evaluate difference
+					Subtraction mySubtraction;
+					string value = mySubtraction.evaluate(*left, *right);
+					int len1=b-0;//size
+					int len2=INPUT.size()-c;//size
+					//Create substring of remainder of expression
+					INPUT=INPUT.substr(0,len1)+value+INPUT.substr(c,len2);
+				}
+
+
+			}
+		}
+
+    }
 
     for(int i=1; i<INPUT.size();i++){
-        if ((INPUT.substr(i,1)=="+") || (INPUT.substr(i,1)=="-") || (INPUT.substr(i,1)=="*") || (INPUT.substr(i,1)=="/") || (INPUT.substr(i,1)==")")){//check symbol
-            string *equation1;//pointer
-            equation1=&INPUT;//pointer
-            ArithmeticExpression test;//object
-            test.evaluate(*equation1);//evaluate
+		//if the character inspected is an operator or a right bracket (+, -, *, /, ))
+        if ((INPUT.substr(i,1)=="+") || (INPUT.substr(i,1)=="-") || (INPUT.substr(i,1)=="*") || (INPUT.substr(i,1)=="/") || (INPUT.substr(i,1)==")")){
+            string *equation1;
+            equation1=&INPUT;
+			
+			//evaluate entire expression
+            ArithmeticExpression test;
+            test.evaluate(*equation1);
+			
+			//deallocate memory
             equation1=NULL;
             delete equation1;
 
@@ -329,14 +360,15 @@ string ArithmeticExpression::evaluate(string &INPUT){
         INPUT="0";
     }
 
+	//Deallocate memory
     left=NULL;
     delete left;
     right=NULL;
     delete right;
 
-    return INPUT;//returb input
-}//end
-ArithmeticExpression::~ArithmeticExpression(){//deconstructor
+    return INPUT;
 }
+//Deallocate memory
+ArithmeticExpression::~ArithmeticExpression(){}
 
 
